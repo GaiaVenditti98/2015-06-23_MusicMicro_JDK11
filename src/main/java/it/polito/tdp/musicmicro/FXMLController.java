9b5d766cf815c8f401e72d6342d5a5a987f8b916
s244926 <s244926@studenti.polito.it>
2020-06-07 23:07:36 +0200

    Progetto Base turno B

--- src/main/java/it/polito/tdp/musicmicro/FXMLController.java
+++ src/main/java/it/polito/tdp/musicmicro/FXMLController.java
@@ -9,36 +9,36 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
-//controller turno A --> switchare al branch master_turnoB o master_turnoC per turno B o turno C
+//controller turno B --> switchare al branch master_turnoA o master_turnoC per turno A o turno C
 
 public class FXMLController {
 
 	private Model model;
 	
-    @FXML
+	@FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
 
     @FXML
-    private ComboBox<?> boxMese;
+    private ComboBox<?> boxGiorno;
 
     @FXML
-    private Button btnArtisti;
+    private Button btnCitta;
 
     @FXML
-    private Button btnNazioni;
+    private Button btnBraniComuni;
 
     @FXML
     private TextArea txtResult;
 
     @FXML
     void initialize() {
-        assert boxMese != null : "fx:id=\"boxMese\" was not injected: check your FXML file 'MusicA.fxml'.";
-        assert btnArtisti != null : "fx:id=\"btnArtisti\" was not injected: check your FXML file 'MusicA.fxml'.";
-        assert btnNazioni != null : "fx:id=\"btnNazioni\" was not injected: check your FXML file 'MusicA.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'MusicA.fxml'.";
+        assert boxGiorno != null : "fx:id=\"boxGiorno\" was not injected: check your FXML file 'MusicB.fxml'.";
+        assert btnCitta != null : "fx:id=\"btnCitta\" was not injected: check your FXML file 'MusicB.fxml'.";
+        assert btnBraniComuni != null : "fx:id=\"btnBraniComuni\" was not injected: check your FXML file 'MusicB.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'MusicB.fxml'.";
 
     }
 
